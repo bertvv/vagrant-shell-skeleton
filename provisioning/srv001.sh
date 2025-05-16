162ff8d075eebbf96ceb89aa7946ed8071b9799a
Bert Van Vreckem <bert.vanvreckem@hogent.be>
2025-05-16 19:48:51 +0200

    Fix mistake in variable initialization

--- provisioning/srv001.sh
+++ provisioning/srv001.sh
@@ -16,9 +16,9 @@ set -o pipefail  # don't mask errors in piped commands
 #------------------------------------------------------------------------------
 
 # Location of provisioning scripts and files
-export PROVISIONING_SCRIPTS="/vagrant/provisioning/"
+readonly PROVISIONING_SCRIPTS="/vagrant/provisioning/"
 # Location of files to be copied to this server
-export PROVISIONING_FILES="${PROVISIONING_SCRIPTS}/files/${HOSTNAME}"
+readonly PROVISIONING_FILES="${PROVISIONING_SCRIPTS}/files/${HOSTNAME}"
 
 export PROVISIONING_SCRIPTS PROVISIONING_FILES
 
